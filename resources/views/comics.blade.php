99327bf6c8682b37f8c6d14c385b83e6ccffae13
NicoloYan <nico.yan@outlook.it>
2022-08-24 15:25:20 +0200

    comics section template

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('main_content')
-    <h1>lista prodotti</h1>
+    <div class="content_section">
+        <span class="current_series">CURRENT SERIES</span>
+        <div class="container">
+
+            <!-- Single Card -->
+            <div class="card">
+                <img src="" alt="">
+                <h3></h3>
+            </div>
+
+        </div>
+        <div class="load_more">
+            <div class="load_more_btn">
+                LOAD MORE
+            </div>
+        </div>
+    </div>
 @endsection

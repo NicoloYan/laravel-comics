0e7bc986f57e802c5cd01022c3f7bc156509ab10
NicoloYan <nico.yan@outlook.it>
2022-08-24 14:06:45 +0200

    Layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <!-- Header goes here -->
+
+    <!-- Jumbotron goes here -->
+    
+    <main>
+        @yield('main_content')
+    </main>
+
+    <!-- Footer goes here -->
+</body>
+</html>
---
+++ resources/views/products.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('main_content')
+    <h1>lista prodotti</h1>
+@endsection

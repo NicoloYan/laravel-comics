35c8584108b82a906b0f944c839679c5f767f5e3
NicoloYan <nico.yan@outlook.it>
2022-08-25 15:56:25 +0200

    comics section sorted

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -19,4 +19,54 @@
             </div>
         </div>
     </div>
+
+    <div class="cta_section">
+        <div class="container">
+
+            <div class="cta_card">
+                <div class="card_img">
+                    <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="digital comics">
+                </div>
+                <div class="cta_text">
+                    DIGITAL COMICS
+                </div>
+            </div>
+
+            <div class="cta_card">
+                <div class="card_img">
+                    <img src="{{ asset('img/buy-comics-merchandise.png') }}" alt="digital comics">
+                </div>
+                <div class="cta_text">
+                    DC MERCHANDISE
+                </div>
+            </div>
+
+            <div class="cta_card">
+                <div class="card_img">
+                    <img src="{{ asset('img/buy-comics-subscriptions.png') }}" alt="digital comics">
+                </div>
+                <div class="cta_text">
+                    SUBSCRIPTION
+                </div>
+            </div>
+
+            <div class="cta_card">
+                <div class="card_img">
+                    <img src="{{ asset('img/buy-comics-shop-locator.png') }}" alt="digital comics">
+                </div>
+                <div class="cta_text">
+                    COMIC SHOP LOCATOR
+                </div>
+            </div>
+
+            <div class="cta_card">
+                <div class="card_img">
+                    <img src="{{ asset('img/buy-dc-power-visa.svg') }}" alt="digital comics">
+                </div>
+                <div class="cta_text">
+                    DC POWER VISA
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection

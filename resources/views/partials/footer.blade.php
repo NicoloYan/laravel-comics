1de23f1affff19a49afe15ac1bb85f421ce1e4ea
NicoloYan <nico.yan@outlook.it>
2022-08-25 16:15:08 +0200

    footer done

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,150 @@
 <footer>
-    FOOTER
+    {{-- FOOTER TOP --}}
+    <div class="footer_top">
+        <div class="container">
+
+            <!-- LEFT COL -->
+            <div class="footer_col">
+
+                <h3>DC COMICS</h3>
+                <ul>
+                    <li>
+                        <a href="#">Characters</a>
+                    </li>
+                    
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">Games</a>
+                    </li>
+                    <li>
+                        <a href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                </ul>
+
+                <h3>SHOP</h3>
+
+                <ul>
+                    <li>
+                        <a href="#">Shop DC</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop DC Collectibles</a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- MIDDLE COL -->
+            <div class="footer_col">
+                
+                <h3>DC</h3>
+                <ul>
+                    <li>
+                        <a href="#">Terms Of Use</a>
+                    </li>
+                    
+                    <li>
+                        <a href="#">Privacy policy (New)</a>
+                    </li>
+                    <li>
+                        <a href="#">Ad Choices</a>
+                    </li>
+                    <li>
+                        <a href="#">Advertising</a>
+                    </li>
+                    <li>
+                        <a href="#">Jobs</a>
+                    </li>
+                    <li>
+                        <a href="#">Subscriptions</a>
+                    </li>
+                    <li>
+                        <a href="#">Talent Workshops</a>
+                    </li>
+                    <li>
+                        <a href="#">CPSC Certificates</a>
+                    </li>
+                    <li>
+                        <a href="#">Ratings</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop Help</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+
+            </div>
+
+            <!-- RIGHT COL -->
+            <div class="footer_col">
+                
+                <h3>SITES</h3>
+                <ul>
+                    <li>
+                        <a href="#">DC</a>
+                    </li>
+                    
+                    <li>
+                        <a href="#">MAD Magazine</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Kids</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Universe</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Power Visa</a>
+                    </li>
+                    
+                </ul>
+
+            </div>
+
+            <div class="footer-logo">
+                <img src="{{ asset('img/dc-logo-bg.png') }}" alt="">
+            </div>
+        </div>
+    </div>
+
+
+    {{-- FOOTER BOTTOM --}}
+
+    <div class="footer_bottom">
+        <div class="container">
+            <div class="btn">
+                <a href="#">SIGN-UP NOW!</a>
+            </div>
+            <div class="footer_links">
+                <span>FOLLOW US</span>
+                <div class="social_image">
+                    <img src="{{ asset('img/footer-facebook.png') }}" alt="">
+                </div>
+                <div class="social_image">
+                    <img src="{{ asset('img/footer-twitter.png') }}" alt="">
+                </div>
+                <div class="social_image">
+                    <img src="{{ asset('img/footer-youtube.png') }}" alt="">
+                </div>
+                <div class="social_image">
+                    <img src="{{ asset('img/footer-pinterest.png') }}" alt="">
+                </div>
+                <div class="social_image">
+                    <img src="{{ asset('img/footer-periscope.png') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
